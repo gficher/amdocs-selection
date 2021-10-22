5b9aac10af83fd621747db84cc479527cbfe8770
Gustavo Ficher <gustavoficher@hotmail.com>
2021-10-22 00:03:40 -0300

    Add coordinate checks to Plateau

--- src/com/gficher/amdocs/Plateau.java
+++ src/com/gficher/amdocs/Plateau.java
@@ -27,6 +27,27 @@ public class Plateau {
         return newRover;
     }
 
+    public boolean hasRoverInCoordinate(Coordinate coord) {
+        for (Rover rover : roverList) {
+            if (coord.hasSameCoordinate(rover.getCoordinate())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isCoordWithinBounds(Coordinate coord) {
+        // Check X coordinate
+        if (coord.getX() < 0) return false;
+        if (coord.getX() >= this.xSize) return false;
+
+        // Check Y coordinate
+        if (coord.getY() < 0) return false;
+        if (coord.getY() >= this.ySize) return false;
+
+        return true;
+    }
+
 
     @Override
     public String toString() {
